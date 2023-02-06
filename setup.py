df53138122f7253442fa4b0e539f3b274ea6c353
maov <maov@gmail.com>
2023-02-06 11:42:08 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = ['pyyaml', 'opencv-python', 'torch', 'torchvision', 'requests', 'wget', 'tqdm', 'ultralytics']
+
+setuptools.setup(
+    name="det_executor",
+    version="0.0.2",
+    author="Maxim Volkovskiy",
+    author_email="maxwolf8852@gmail.com",
+    description="Python package with latest versions of YOLO architecture for training and inference",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxwolf8852/DetExecutor.git",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
